fc047a95221d07a6263d10ae41f3c12d85859597
laetitia123 <uwizelaetitia@gmail.com>
2021-07-30 15:42:18 +0200

    setting privileges on user model

--- server/model/user.js
+++ server/model/user.js
@@ -1,11 +1,32 @@
 import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
-    firstName:String,
+     firstName: {
+        type: String,
+        required: [true, "first-name is required"]
+    },
     lastname:String,
-    email:String,
-    password:String,
+     email: {
+        type: String,
+        unique: true
+    },
+     password: { 
+        type: String, 
+        default: "12345@@@" },
     phone:String,
-    gender:String
+      gender: {
+        type: String,
+        enum: ["male", "female"]
+    },
+     role:{
+        type: String,
+        enum:["admin","mentor","user"],
+        default:"user"
+    },
+    status:{
+        type: String,
+        enum:["active","inactive"],
+        default:"active"
+    }
 });
 const UserInfos = mongoose.model("user" ,UserSchema);
 
